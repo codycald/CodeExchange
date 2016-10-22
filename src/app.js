f949d281e6b226db55cf8bfde0bcecc3d0f116b8
Cody <cocaldwe@gmail.com>
2016-10-22 11:42:26 +0000

    Basic Angular and Bootstrap setup

---
+++ src/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log('Server started');
+});
