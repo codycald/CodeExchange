31ba1216fa587b278a4210c802c64042ea84eb37
Cody <cocaldwe@gmail.com>
2016-10-24 06:07:00 +0000

    Added frontend functionality to comment/answer questions

--- public/scripts/controllers/question.js
+++ public/scripts/controllers/question.js
@@ -2,4 +2,21 @@
 angular.module('stackoverflowclone')
 .controller('questionCtrl', function($scope, dataService, $routeParams) {
     $scope.question = dataService.getQuestionById($routeParams.id);
+    
+    $scope.submitQuestionComment = function(qaPost) {
+        if ($scope.textArea)
+            qaPost.comments.push({text: $scope.textArea, author: 'Author 1'});
+        $scope.textArea = '';
+    }
+    
+    $scope.submitAnswer = function(question) {
+        if ($scope.answerText)
+            question.answers.push({ text: $scope.answerText, 
+                                    author: 'Author 1',
+                                    votes: 0,
+                                    comments: []
+                                 });
+        $scope.answerText = '';
+    }
+    
 });
